f84bfdb174103ace45f036c264d6c97f9780aee9
Jeongyeon_Song <songj239587@gmail.com>
2026-03-13 16:00:42 +0900

    :: project setting

---
+++ src/main/java/fiveonestudy/ddait/DdaitApplication.java
@@ -0,0 +1,13 @@
+package fiveonestudy.ddait;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdaitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdaitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fiveonestudy/ddait/DdaitApplicationTests.java
@@ -0,0 +1,13 @@
+package fiveonestudy.ddait;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdaitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
