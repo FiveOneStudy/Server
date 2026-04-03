00a52386c3e8c1181dde0197b88ecf3f2f35b5b0
leesh <leesh6541@gmail.com>
2026-04-03 12:24:43 +0900

    add: user domain, role, repository

---
+++ src/main/java/fiveonestudy/ddait/user/dto/UserSignUpDto.java
@@ -0,0 +1,13 @@
+package fiveonestudy.ddait.user.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class UserSignUpDto {
+
+    private String email;
+    private String password;
+    private String nickname;
+}
