a0ac0ea6cfa63632f540612caff9fff3047c5df9
Jeongyeon_Song <songj239587@gmail.com>
2026-04-06 13:51:39 +0900

    modify: Annotation

--- src/main/java/fiveonestudy/ddait/plan/dto/PlanRequest.java
+++ src/main/java/fiveonestudy/ddait/plan/dto/PlanRequest.java
@@ -2,10 +2,12 @@ package fiveonestudy.ddait.plan.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class PlanRequest {
     private LocalDate date;
