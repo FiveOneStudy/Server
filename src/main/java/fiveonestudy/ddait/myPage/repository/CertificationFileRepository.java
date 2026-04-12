5a5832175890d5053377f3ca9734004340095f87
leesh <leesh6541@gmail.com>
2026-04-13 00:57:09 +0900

    modify: create deleteByUserCertification method

--- src/main/java/fiveonestudy/ddait/myPage/repository/CertificationFileRepository.java
+++ src/main/java/fiveonestudy/ddait/myPage/repository/CertificationFileRepository.java
@@ -1,7 +1,9 @@
 package fiveonestudy.ddait.myPage.repository;
 
 import fiveonestudy.ddait.myPage.entity.CertificationFile;
+import fiveonestudy.ddait.myPage.entity.UserCertification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CertificationFileRepository extends JpaRepository<CertificationFile, Long> {
+    CertificationFile deleteByUserCertification(UserCertification userCertification);
 }
