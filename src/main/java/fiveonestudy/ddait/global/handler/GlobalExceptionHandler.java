67d185b357f02eb0d3d8a670b522ec54e1df67fd
leesh <leesh6541@gmail.com>
2026-04-12 18:05:49 +0900

    modify: use Lombok

--- src/main/java/fiveonestudy/ddait/global/handler/GlobalExceptionHandler.java
+++ src/main/java/fiveonestudy/ddait/global/handler/GlobalExceptionHandler.java
@@ -1,12 +1,13 @@
 package fiveonestudy.ddait.global.handler;
 
 import fiveonestudy.ddait.global.response.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Map;
-
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
