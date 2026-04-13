0185ac76b021702947cdf2c6d4b3f1f5f6b77395
leesh <leesh6541@gmail.com>
2026-04-13 09:50:08 +0900

    modify: create findByUserCertificationId

--- src/main/java/fiveonestudy/ddait/myPage/repository/CertificationFileRepository.java
+++ src/main/java/fiveonestudy/ddait/myPage/repository/CertificationFileRepository.java
@@ -4,6 +4,9 @@ import fiveonestudy.ddait.myPage.entity.CertificationFile;
 import fiveonestudy.ddait.myPage.entity.UserCertification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CertificationFileRepository extends JpaRepository<CertificationFile, Long> {
     CertificationFile deleteByUserCertification(UserCertification userCertification);
+    Optional<CertificationFile> findByUserCertificationId(Long userCertificationId);
 }
