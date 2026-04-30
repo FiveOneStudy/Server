8e19a5859260dc9dc87a831a9ad2495c0dc09bce
leesh <leesh6541@gmail.com>
2026-04-30 10:13:48 +0900

    modify: repository enhancements

--- src/main/java/fiveonestudy/ddait/community/repository/CommentRepository.java
+++ src/main/java/fiveonestudy/ddait/community/repository/CommentRepository.java
@@ -1,11 +1,16 @@
 package fiveonestudy.ddait.community.repository;
 
 import fiveonestudy.ddait.community.entity.Comment;
+import fiveonestudy.ddait.community.entity.CommentStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByPostIdOrderByIdAsc(Long postId);
-    List<Comment> findByUserId(Long userId);
+    List<Comment> findByUserIdOrderByIdDesc(Long userId);
+    Optional<Comment> findByIdAndPostId(Long postId, Long id);
+
+    List<Comment> findAllByStatus(CommentStatus status);
 }
