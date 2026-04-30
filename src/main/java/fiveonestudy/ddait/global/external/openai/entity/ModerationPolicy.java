292840c4d91c450f4709a4bf74359578d09dac01
leesh <leesh6541@gmail.com>
2026-04-30 10:16:19 +0900

    add: ModerationPolicy with predefined thresholds

---
+++ src/main/java/fiveonestudy/ddait/global/external/openai/entity/ModerationPolicy.java
@@ -0,0 +1,9 @@
+package fiveonestudy.ddait.global.external.openai.entity;
+
+public class ModerationPolicy {
+    public static final double VIOLENCE_THRESHOLD = 0.7;
+    public static final double SEXUAL_THRESHOLD = 0.7;
+    public static final double SELF_HARM_THRESHOLD = 0.5;
+    public static final double HARASSMENT_THRESHOLD = 0.8;
+
+}
