b1f94570347d959e10419d7df7564cf9bb294373
leesh <leesh6541@gmail.com>
2026-04-30 10:15:22 +0900

    modify: remove deleted field

--- src/main/java/fiveonestudy/ddait/community/dto/CommentResponse.java
+++ src/main/java/fiveonestudy/ddait/community/dto/CommentResponse.java
@@ -12,15 +12,13 @@ public class CommentResponse {
     private String content;
     private String writer;
     private Long parentId;
-    private boolean deleted;
 
     public static CommentResponse from(Comment comment) {
         return CommentResponse.builder()
                 .commentId(comment.getId())
-                .content(comment.isDeleted() ? "삭제된 댓글입니다." : comment.getContent())
+                .content(comment.getContent())
                 .writer(comment.getUser().getNickname())
                 .parentId(comment.getParent() != null ? comment.getParent().getId() : null)
-                .deleted(comment.isDeleted())
                 .build();
     }
 }
