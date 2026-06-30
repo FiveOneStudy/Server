fb2123d57d26b0b82a40e08525b9b5b8e8e92f27
Jeongyeon_Song <songj239587@gmail.com>
2026-06-30 14:57:21 +0900

    modify: id add

---
+++ src/main/java/fiveonestudy/ddait/plan/dto/PlanDeleteRequest.java
@@ -0,0 +1,10 @@
+package fiveonestudy.ddait.plan.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PlanDeleteRequest {
+    private Long planId;
+}
