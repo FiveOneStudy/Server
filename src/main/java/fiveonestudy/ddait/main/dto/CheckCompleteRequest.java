1dd0c94cf7ff1c8c7255c7e92c424c1fddac6cf9
Jeongyeon_Song <songj239587@gmail.com>
2026-06-30 15:09:07 +0900

    modify: id logic

--- src/main/java/fiveonestudy/ddait/main/dto/CheckCompleteRequest.java
+++ src/main/java/fiveonestudy/ddait/main/dto/CheckCompleteRequest.java
@@ -2,14 +2,11 @@ package fiveonestudy.ddait.main.dto;
 
 import lombok.*;
 
-import java.time.LocalDate;
-
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class CheckCompleteRequest {
-    private LocalDate date;
-    private String content;
+    private Long checkId;
 }
