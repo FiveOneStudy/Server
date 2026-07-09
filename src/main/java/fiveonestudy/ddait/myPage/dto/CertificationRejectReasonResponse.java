35ec8b1c463a7ad2fec7d6acdb1b44a75347bd84
leesh <leesh6541@gmail.com>
2026-07-09 17:06:06 +0900

    add: certification reject reason DTO

---
+++ src/main/java/fiveonestudy/ddait/myPage/dto/CertificationRejectReasonResponse.java
@@ -0,0 +1,11 @@
+package fiveonestudy.ddait.myPage.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CertificationRejectReasonResponse {
+
+    private String reason;
+}
